3b7153e534bf7561e1ca76a8e1cad8da5945a99f
Tony Chang <tony@eliminated.org>
2024-08-12 21:12:22 -0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Tony Chang on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
